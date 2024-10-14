5968341effbf0609c283e6945c854acd3703d0b8
msun97 <godqhr9782@naver.com>
2024-10-14 23:41:52 +0900

    241014 home

---
+++ frontend-01/04-JavaScript/JS/01-launch.js
@@ -0,0 +1,27 @@
+// error 예시
+
+// const $counter=document.getElementById('counter');
+// const $increase=document.getElementById('increase');
+// const $decrease=document.getElementById('decrease');
+
+// let num = 0;
+// const render = function(){$counter.innerHTML=num;};
+
+// $increase.onclick=function(){
+//     num++;
+//     console.log('increase 버튼 클릭', num);
+//     render();
+// };
+
+// $decrease.onclick=function(){
+//     num--;
+//     console.log('decrease 버튼 클릭', num);
+//     render();
+// };
+
+
+// const arr=[1,2,3];
+// arr.forEach(console.log);
+
+const arr=[1,2,3];
+arr.forEach(alert);
